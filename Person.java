dbe292da6ac20e9ee5602ddbddc4953994bde9f7
Hazel Azka <hazelazka091@gmail.com>
2024-12-10 21:09:55 +0700

    Person.java

---
+++ Person.java
@@ -0,0 +1,38 @@
+package School 2;
+
+public class Person {
+    
+    public String name;
+    public int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+        name = "";
+        age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void print() {
+        System.out.println("Nama: " + name);
+        System.out.println("Umur: " + age + " tahun");
+    }
+}
